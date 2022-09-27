6b3ddbb534df349e357b5afe2b015ce90484b4da
Erick Gustavo <erick.stazs@gmail.com>
2022-09-27 10:42:46 -0300

    Add counter

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react'
+
 function App () {
+  const [counter, setCounter] = useState(0)
+
+  function handleIncrement () {
+    setCounter(counter => counter + 1)
+  }
+
+  function handleDecrement () {
+    setCounter(counter => counter - 1)
+  }
+
   return (
-    <h1>App</h1>
+    <>
+      <h1>{counter}</h1>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleIncrement}>+</button>
+    </>
   )
 }
 
