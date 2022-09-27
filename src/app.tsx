b8f4e5e2177d6ecc2953b939b76c897d93adbe8f
Erick Gustavo <erick.stazs@gmail.com>
2022-09-27 10:18:13 -0300

    Add eslint with standard styleguide

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-function App() {
+function App () {
   return (
     <h1>App</h1>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import './index.css'
@@ -10,7 +10,7 @@ if (!rootElement) {
 }
 
 createRoot(rootElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 )
